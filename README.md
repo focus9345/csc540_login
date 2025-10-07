3a52a630f769aed9b40829dda1d2210deea467ac
focus9345 <connor.joshua@gmail.com>
2025-10-07 14:37:52 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# CSC540 Login
+A basic database web application that just focuses on a simple login.
+
+## Stack
+The project was built using [xampp] (https://www.apachefriends.org/index.html)
+- Appache
+- MariaDB
+- PHP
+- Pearl
+
+## INSTRUCTIONS
+These files must be hosted within a system that supports lamp stacks, preferably instal this into a XAMPP and run locally. This project is not built to scale for a production based website, it intention is to be used as a teaching tool.
---
+++ template.php
@@ -0,0 +1,16 @@
+<!-- A template file for all pages to use just copy this page to make future pages -->
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- One head file for all pages -->
+    <?php include_once "./include/head.php"; ?>
+  </head>
+  <body>
+    <?php include_once "./include/header.php"; ?>
+    <main role="main" class="container">
+      <!-- Primary Content Goes Here -->
+    </main>
+    <!-- One footer file for all pages -->
+    <?php include_once "./include/footer.php"; ?>
+  </body>
+</html>
